6aa8dd59d9343c6f41ac002f417e5fde3284dd39
AvantAveGarde <coltonlee4407@hotmail.com>
2019-09-08 01:31:20 -0600

    Crab AI and Aggro

---
+++ Sandy_Move_Proto/Assets/_Scripts/Aggro_AOE.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aggro_AOE : MonoBehaviour
+{
+    public Crab parent;
+    public GameObject FOV_Range;
+    public GameObject Aggro_Radius;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            parent.isInAggroRange = true;
+            parent.Player = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FOV_Range.SetActive(true);
+            Aggro_Radius.SetActive(false);
+        }
+    }
+}
---
+++ Sandy_Move_Proto/Assets/_Scripts/Aggro_Range.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aggro_Range : MonoBehaviour
+{
+    public Crab parent;
+    public GameObject FOV_Range;
+    public GameObject Aggro_Radius;
+    private float aggroDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aggroDistance = Aggro_Radius.GetComponent<SphereCollider>().radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            parent.isInAggroRange = true;
+            parent.Player = other.gameObject;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            print(Vector3.Distance(transform.position, other.transform.position));
+            if (Vector3.Distance(transform.position, other.transform.position) <= aggroDistance)
+            {
+                FOV_Range.SetActive(false);
+                Aggro_Radius.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            parent.isInAggroRange = false;
+            parent.Player = null;
+        }
+    }
+}
